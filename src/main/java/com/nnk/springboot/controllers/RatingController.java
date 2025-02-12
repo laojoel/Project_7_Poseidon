f5ab0193e0628daaefd53b390d7940907c0e0ff9
Joel LAO <joel.lao@axa.fr>
2025-02-12 15:37:51 +0100

    rating section

--- src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -9,10 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 //import javax.validation.Valid;
 
@@ -34,27 +33,36 @@ public class RatingController {
     }
 
     @PostMapping("/rating/validate")
-    public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+    public String validate(@Valid @ModelAttribute("rating") Rating rating, BindingResult result, Model model, HttpServletRequest request) {
+        if (result.hasErrors()) {return "rating/add";}
+        ratingService.save(rating);
+        model.addAttribute("ratings", ratingService.loadAll());
+        model.addAttribute("remoteUser", request.getRemoteUser());
+        return "rating/list";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
-        return "rating/update";
-    }
-
-    @PostMapping("/rating/update/{id}")
-    public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
+        Optional<Rating> rating = ratingService.load(id);
+        if (rating.isPresent()) {
+            model.addAttribute("rating", rating.get());
+            return "rating/update";
+        }
         return "redirect:/rating/list";
     }
 
+    @PostMapping("/rating/update/{id}")
+    public String updateRating(@Valid @ModelAttribute("rating") Rating rating, BindingResult result, Model model, HttpServletRequest request) {
+        if (result.hasErrors()) {return "rating/update";}
+        ratingService.save(rating);
+        model.addAttribute("ratings", ratingService.loadAll());
+        model.addAttribute("remoteUser", request.getRemoteUser());
+        return "rating/list";
+    }
+
     @GetMapping("/rating/delete/{id}")
-    public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+    public String deleteRating(@PathVariable("id") Integer id) {
+        ratingService.delete(id);
         return "redirect:/rating/list";
     }
 }
